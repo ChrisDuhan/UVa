aa4349df44b55edec8db496988a053bd0f719259
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2018-09-08 13:57:54 -0600

    Update 11192-Group Reverse.cpp

--- 11192-Group Reverse.cpp
+++ 11192-Group Reverse.cpp
@@ -1,24 +1,32 @@
+//////////////////////////////////////////////////////////////////////
+//
+// Chris Duhan: Group Reverse: 11192: chriscodes
+//
+// Data structure required : STL string
+//
+// Tricks of the trade: Use the reverse function with some clever 
+//						indexing.
+//
+/////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <string>
+#include <algorithm> // reverse()
 
 using namespace std;
 
 int main() {
-  int c = 0, len = 0, t = 0;
-  string s;
-  cin >> c;
-  while (c != 0) {
-    cin >> s;
-    len = s.length();
-    t = len/c;
-    char buffer[t];
-    for (int i = 1; i <= c; ++i) {
-      s.copy(buffer,t,c*i);
-    }
-
-
-
-    cin >> c;
-  }
-  return 0;
+	int c = 0, group = 0, sos = 0; // sos = start of set
+	string s;
+	cin >> c;
+	while (c != 0) {
+		cin >> s;
+		group = s.length() / c;
+		for (int i = 1; i <= c; i++) {
+			sos = group * i;
+			reverse(s.begin()+(sos-group), s.begin()+(sos));
+		}
+		cout << s << endl;
+		cin >> c;
+	}
+	return 0;
 }
