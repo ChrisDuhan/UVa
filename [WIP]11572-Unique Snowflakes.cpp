0af59b8dd9ab6b1c81d519a839ff4486cef863ec
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2018-10-11 13:08:05 -0500

    Create [WIP]11572-Unique Snowflakes.cpp

---
+++ [WIP]11572-Unique Snowflakes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int main() {
+	int t, n;
+	cin >> t;
+	for (int i = 0; i < t; ++i) {
+		cin >> n;
+		for (int j = 0; j < n; ++j) {
+
+		}
+	}
+
+	return 0;
+}
