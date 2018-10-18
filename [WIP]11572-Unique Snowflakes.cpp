539833e75682373670ceeafdf3622543c1275e05
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2018-10-18 11:06:51 -0500

    Completed

--- [WIP]11572-Unique Snowflakes.cpp
+++ [WIP]11572-Unique Snowflakes.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-	int t, n;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	unordered_map<int, int> map;
+	int t, n, input;
 	cin >> t;
 	for (int i = 0; i < t; ++i) {
 		cin >> n;
+		int testLen, cnt, longest;
+		testLen = cnt = longest = 0;
+		map.clear();
 		for (int j = 0; j < n; ++j) {
-
+			cin >> input;
+			testLen = max(testLen, map[input]);
+			map[input] = ++cnt;
+			longest = max(longest, cnt - testLen);
 		}
+		cout << longest << '\n';
 	}
-
 	return 0;
 }
