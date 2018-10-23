e391f4df05fc9daf80a758e6ec9dae794c2f453e
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2018-10-22 23:59:22 -0500

    Update [WIP]834-Continued Fractions.cpp

--- [WIP]834-Continued Fractions.cpp
+++ [WIP]834-Continued Fractions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,17 +7,23 @@ int main() {
 	int num, den;
 	while (cin >> num >> den) {
 		cout << '[';
-		div_t divresult;
-		divresult = div(num, den);
-		cout << divresult.quot << ';';
-		while (divresult.rem != 1) {
+		vector<int> s;
+		while (num % den) {
+			s.push_back(num / den);
+			int tmp = num;
 			num = den;
-			den = divresult.rem;
-			divresult = div(num, den);
-			cout << divresult.quot << ',';
+			den = tmp % den;
 		}
-		cout << den << "]\n";
-
+		s.push_back(num / den);
+		cout << s[0];
+		for (int i = 1; i < s.size(); ++i) {
+			if (i == 1)	
+				cout << ';';
+			else
+				cout << ',';
+			cout << s[i];
+		}
+		cout << "]\n";
 	}
 	return 0;
 }
