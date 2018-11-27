e62bc3aa4e71a0c44fcce97505a5152c834bd2f9
Christopher Duhan <ChrisDuhan@users.noreply.github.com>
2018-11-26 20:15:03 -0600

    Finished

--- 836-Largest Submatrix.cpp
+++ 836-Largest Submatrix.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,6 +16,8 @@ int main() {
 		for (int j = 0; j < size; ++j) {
 			for (int k = 0; k < size; ++k) {
 				v[j][k] = s[k] - 48;
+				if (s[k] - 48 == 0)
+					v[j][k] = -625;
 			}
 			if (j != (size - 1)) {
 				cin >> s;
@@ -30,34 +33,37 @@ int main() {
 				v[j][k] = v[j][k] + v[j][k - 1] + v[j - 1][k] - v[j - 1][k - 1];
 			}
 		}
-		int r = 0, c = 0;
-		for (int mr = 1; mr < size; ++mr) {
-			for (int mc = 1; mc < size; ++mc) {
-				while (mr + r < size) {
-					while (mc + c < size) {
-
-
-						++mc;
+		int maxSub = 0;
+		for (int subRow = 0; subRow < size; ++subRow) {
+			for (int subCol = 0; subCol < size; ++subCol) {
+				for (int endRow = subRow; endRow < size; ++endRow) {
+					for (int endCol = subCol; endCol < size; ++endCol) {
+						int currentSub = v[endRow][endCol];
+						if (subRow > 0) // only do when not in a cell in the first row
+							currentSub -= v[subRow - 1][endCol];
+						if (subCol > 0) // only do when not in a cell in the first column
+							currentSub -= v[endRow][subCol - 1];
+						if (subRow > 0 && subCol > 0) // do for every cell not in the first row or column
+							currentSub += v[subRow - 1][subCol - 1];
+						maxSub = max(maxSub, currentSub);
 					}
-					++mr;
 				}
-
 			}
 		}
 
+		cout << maxSub << "\n";
+		if (i != n - 1)
+			cout << "\n";
 
-
-
+		// uncomment below for visualizing
+		/*
 		for (int j = 0; j < size; ++j) {
 			for (int k = 0; k < size; ++k) {
 				cout << v[j][k] << " ";
 			}
 			cout << '\n';
 		}
-		
-
-
-
+		*/
 
 	}
 	return 0;
